50dc6debad38d2b070867f7e4785e5eff444a5bf
Kihhmaster <kihhmaster@yandex.ru>
2020-11-25 22:48:44 +0300

    Add Function

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 'use strict';
+let money, time;
 
-let money = +prompt("Ваш бюджет на месяц?", ''),
-		time = prompt('Введите дату в формате YYYY-MM-DD', '');
+function start()	{
+	money = +prompt("Ваш бюджет на месяц?", '');
+	time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+	while(isNaN(money) || money == "" || money == null) {
+		money = +prompt("Ваш бюджет на месяц?", '');
+
+	}
+}
+start();
+
 
 let appData = {
 	budget: money,
@@ -9,79 +19,70 @@ let appData = {
 	optionalExpenses: {},
 	income: [],
 	timeData: time,
-	savings: false
+	savings: true
 };
 
 
 
 
-for ( let i = 0; i < 2; i++) {
-	let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
-			b = prompt("Во сколько обойдется?", '');
+function chooseeExpenses() {
+	for ( let i = 0; i < 2; i++) {
+		let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+				b = prompt("Во сколько обойдется?", '');
+		
+		if ((typeof(a))=== 'string' && (typeof(a)) !=null && (typeof(b)) !=null
+			&& a != '' && b != '' && a.length < 50) {
+				console.log("done")
 	
-	if ((typeof(a))=== 'string' && (typeof(a)) !=null && (typeof(b)) !=null
-		&& a != '' && b != '' && a.length < 50) {
-			console.log("done")
+			appData.expenses[a] = b;
+		}else {
+			console.log("bad result");
+			i--;
+		}
+	
+	};
+}
 
-		appData.expenses[a] = b;
-	}else {
-		console.log("bad result");
-		i--;
+chooseeExpenses();
+
+function  checkSavings() {
+		if (appData.savings == true) {
+			let save = +prompt("Какова сумма накоплений?"),
+					percent = +prompt ("Под какой процент?");
+
+			appData.monthIncome = save/100/12*percent;
+			alert("Доход в месяц с вашего депозита" + appData.monthIncome);
+		}
 	}
+checkSavings();
 
-};
+function detectDayBudget() {
+	appData.moneyPerDya = (appData.budget / 30).toFixed();
+	alert("Ежедневгый бюджет: " + appData.moneyPerDya + " руб.");
+}
+detectDayBudget();
 
+function detectLevel () {
 
-// let i = 0;
-// while (i < 2) {
-//     let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
-//         b = prompt ("Во сколько обойдется?", "");
+	if(appData.moneyPerDya < 100) {
+		console.log("Минимальный уровень достатка");
+	}else if (appData.moneyPerDya > 100 && appData.moneyPerDya < 2000) {
+		console.log("Средний уровень достатка");
+	}else if (appData.moneyPerDya > 2000) {
+		console.log("Высокий уровень достатка");
+	}else {
+		console.log("Произошла ошибка");
+	}
+	
 
-//     if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
+}
+detectLevel();
 
-//         console.log ("done");
-
-//         appData.expenses[a] = b;
-//     } else {
-//          console.log ("bad result");
-//          i--;
-//     }
-
-//     i++;
-// }
-
-// Используем цикл DO...WHILE
-
-// let i = 0;
-// do {
-//     let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
-//         b = prompt ("Во сколько обойдется?", "");
-
-//     if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
-
-//         console.log ("done");
-
-//         appData.expenses[a] = b;
-//     } else {
-//          console.log ("bad result");
-//          i--;
-//     }
-
-//     i++;
-// }
-// while(i < 2);
-
-
-appData.moneyPerDya = appData.budget / 30;
-alert("Ежедневгый бюджет: " + appData.moneyPerDya);
-
-if(appData.moneyPerDya < 100) {
-	console.log("Минимальный уровень достатка");
-}else if (appData.moneyPerDya > 100 && appData.moneyPerDya < 2000) {
-	console.log("Средний уровень достатка");
-}else if (appData.moneyPerDya > 2000) {
-	console.log("Высокий уровень достатка");
-}else {
-	console.log("Произошла ошибка")
-};
-
+function chooseOptExpenses() {
+	for (let i = 1; i < 3; i++) {
+		let questionOptExpenses = prompt("Статья необязательных расходов?");
+			appData.optionalExpenses[i] = questionOptExpenses;
+			console.log(appData.optionalExpenses);
+	}
+}
+chooseeExpenses();
